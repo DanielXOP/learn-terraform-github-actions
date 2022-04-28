f063c40f98f148b6aba4c15cd070c6fb5f76c545
Daniel git test <danielxop@gmail.com>
2022-04-28 11:35:39 +0000

    Point backend to correct Terraform config organization and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "AGC-Terraform-Demos-For-Oz"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-GitHub-actions"
     }
   }
 }
